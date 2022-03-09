e18ecedaeed7c2cb3695dcd62a2a4378290be0c2
Cheng <cheng.luo@stud.uni-heidelberg.de>
2022-03-09 12:26:24 +0100

    working on test_numerical.py

---
+++ src/test_numerical.py
@@ -0,0 +1,11 @@
+import pytest
+import numerical
+
+my_wf = #define it
+
+def test_num_crl():
+    test1 = numerical.num_crl(my_wf)
+    assert test1 == #desired wf
+
+def test_auto_corr():
+    test2 = numerical.auto_corr()
