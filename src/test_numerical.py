e0185ead516cd61bf2d08e43675a5e36f215ddb4
Cheng <cheng.luo@stud.uni-heidelberg.de>
2022-03-09 12:48:11 +0100

    last commit before merging weird stuff

--- src/test_numerical.py
+++ src/test_numerical.py
@@ -1,11 +1,11 @@
 import pytest
 import numerical
 
-my_wf = #define it
+my_wf = 
 
 def test_num_crl():
     test1 = numerical.num_crl(my_wf)
-    assert test1 == #desired wf
+    assert test1.dtype == np.complex_
 
 def test_auto_corr():
     test2 = numerical.auto_corr()
