c026093d87e35cbbbff5dd9f319be5f5364bb887
Cheng <cheng.luo@stud.uni-heidelberg.de>
2022-03-13 12:28:20 +0100

    Test for cmplx_array works

--- src/test_numerical.py
+++ src/test_numerical.py
@@ -1,11 +1,12 @@
 import pytest
 import numerical
+import numpy as np
 
-my_wf = 
+my_wf = np.loadtxt('../data/nstate_i.t', dtype=float, skiprows=1)
 
-def test_num_crl():
-    test1 = numerical.num_crl(my_wf)
-    assert test1.dtype == np.complex_
+def test_cmplx_array():
+    test1 = numerical.cmplx_array(my_wf)
+    assert test1[0].dtype == np.complex_
 
-def test_auto_corr():
-    test2 = numerical.auto_corr()
+#def test_auto_corr():
+#    test2 = numerical.auto_corr() 
