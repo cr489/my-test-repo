54ac6f9232ca1e041e18323ad0330ce018a28d41
Cheng <cheng.luo@stud.uni-heidelberg.de>
2022-03-13 13:40:47 +0100

    changes notebook and numerical.py

--- src/numerical.py
+++ src/numerical.py
@@ -1,51 +1,49 @@
 import numpy as np
 
 
-def cmplx_array(file):
-    """Function to create an array of complex dtype from numpy.float array\
+def num_crl(wf_n):
+    """Function computes the autocorrelation function from given vectors\
+    and the Discrete Fourier transform
 
     Args:
-        wf_n(numpy array, float): Wave function over time
+        wf_n(numpy array, complex): Wave function over time
 
     Returns:
         numpy array, complex: The wave function complex over time.
-        numpy array, float: The time column as an array.
+        numpy array, complex: The autocorrelation function over time.
+        numpy array, complex: The Discrete Fourier Transformation function over\
+        frequency
     """
 
-    # loading file as a numpy array dtype=float
-    arr_fl = np.loadtxt("../data/nstate_i.t", dtype=float, skiprows=1)
+    # setting up the time vector and deleting it from array
+    time_vc = np.zeros([len(wf_n[0])])
+    time_vc = wf_n[0]
+    wf_n = np.delete(wf_n,[0],axis=0)   
 
-    # storing time column as a vector and droping from arr_fl
-    time = arr_fl[:, 0]
-    arr_fl = np.delete(arr_fl, 0, axis=1)
+    # the lenth of the vector
+    t_wf = len(wf_n[0])
+    p_wf = len(wf_n[:,0])
 
-    # creating numpy array with dtype=float from arr_fl
-    r_arr = arr_fl[:, 0::2]
-    im_arr = arr_fl[:, 1::2]
-    arr_cmplx = 1j * im_arr
-    arr_cmplx += r_arr
-
-    return arr_cmplx, time
+    # turning array into complex
+    comp_vc = np.zeros([p_wf,t_wf],dtype=np.complex_)
+    for n in range(p_wf):
+        comp_vc[:,n] = wf_n[n * 2] + wf_n[1 + n * 2] * 1j
+    return comp_vc, time_vc
 
 
-def auto_corr(arr_cmplx, time):
+def auto_corr(comp_vc,p_wf,t_wf):
     """Function computes the autocorrelation function from given vectors
     Args:
-        arr_cmplx(numpy array, complex): Wave function over time
-        time(numpy array, float): time column
+        wf_n(numpy array, complex): Wave function over time
 
     Returns:
         numpy array, complex: The autocorrelation function over time.
     """
-    t_len = len(time)
-    # creating arrays
-    init_wf = arr_cmplx[0, :]
-    prop_wf = arr_cmplx[:, :]
-    # autocorrelation fuction
-    ac_file = np.zeros([t_len], dtype=np.complex)
-    for i in range(t_len):
-        ac_file[i] = np.correlate(init_wf, prop_wf[i, :])
 
+    # autocorrelation fuction
+    ac_file = np.zeros([p_wf,t_wf + 1],dtype=np.complex_)
+    for n in range(p_wf):
+        ac_file[n] = np.sum(comp_vc[:,0] * np.conjugate(comp_vc[:,n]))
     return ac_file
 
 
@@ -58,13 +56,9 @@ def FFT_wf(ac_file):
         Wave function over time
 
     Returns:
-        numpy array, complex: The Discrete Fourier Transformation function\
-        over frequency
+        numpy array, complex: The Discrete Fourier Transformation function over\
+        frequency
     """
-    FT1_file = np.fft.fft(ac_file)
-    FT1_t = np.fft.fftfreq(len(ac_file))
-
-    FT_file = FT1_file[0:len(ac_file)//2] # Dropping the negative part 
-    FT_t = FT1_t[0:len(ac_file)//2]       # Dropping the negative part
-    
+    FT_file = np.fft.fft(ac_file)
+    FT_t = np.fft.fftfreq(len(ac_file))
     return FT_file, FT_t
