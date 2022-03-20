71cb79c88629df8d2f28692ea2fab15af6415224
Gomezj <johana.gomez@stud.uni-heidelberg.de>
2022-03-20 18:55:40 +0100

    test statistical file-working on it

---
+++ src/test_statistical.py
@@ -0,0 +1,16 @@
+import pytest
+import statistical as std
+import numpy as np
+import pandas as pd
+
+#myfile = np.loadtxt("../data/expec.t", dtype=float, skiprows=1)
+
+class 
+    def __init__(self,filename,thrv1):
+        self.fle1 - filename
+        self.t
+        
+
+    def test_thr(self):
+        fle2 = std.exp_drop(self.fle1,2.0e-5)
+        assert fle2[] =< 0.0001
